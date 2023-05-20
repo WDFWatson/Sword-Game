1eb643f69447806adbe88b9891c552cf2235111e
Watson <wdfwatson@gmail.com>
2023-05-19 21:08:42 -0300

    Added increasing spawning rate

--- Assets/Scripts/SwordSpawner.cs
+++ Assets/Scripts/SwordSpawner.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class SwordSpawner : MonoBehaviour
 {
-    [SerializeField] private float swordSpeed;
+    [SerializeField] private float swordSpeed = 2f;
     [SerializeField] private float minTimeBetweenSpawns = 0.2f;
     [SerializeField] private float maxTimeBetweenSpawns = 2f;
+    [SerializeField] private float minTimeBetweenSpawnsMinimum = 0.1f;
+    [SerializeField] private float maxTimeBetweenSpawnsMinimum = 0.1f;
+    [SerializeField] private float timeBetweenRateIncreases = 5f;
+    [SerializeField] private float timeChangeMaximum = 0.01f;
+    [SerializeField] private float timeChangeMinimum = 0.01f;
     private float timeBetweenSpawns = 1f;
     public SwordController swordPrefab;
+    
     private float timeSinceLastSpawn;
+    private float timeSinceLastIncrease;
+    private float currentMinTimeBetweenSpawns;
+    private float currentMaxTimeBetweenSpawns;
     void Start()
     {
+        timeSinceLastIncrease = 0;
         timeSinceLastSpawn = 0;
+        currentMaxTimeBetweenSpawns = maxTimeBetweenSpawns;
+        currentMinTimeBetweenSpawns = minTimeBetweenSpawns;
     }
     
     void FixedUpdate()
     {
         if (timeSinceLastSpawn > timeBetweenSpawns)
         {
-            var newSword = Instantiate(swordPrefab) as SwordController;
+            var newSword = Instantiate(swordPrefab);
             float angle = Random.Range(-90, 90);
             
             newSword.transform.position = transform.position;
@@ -28,6 +40,22 @@ public class SwordSpawner : MonoBehaviour
             timeSinceLastSpawn = 0;
             timeBetweenSpawns = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
         }
+
+        if (timeSinceLastIncrease > timeBetweenRateIncreases)
+        {
+            timeSinceLastIncrease = 0;
+            minTimeBetweenSpawns -= timeChangeMaximum;
+            if (minTimeBetweenSpawns < minTimeBetweenSpawnsMinimum)
+            {
+                minTimeBetweenSpawns = minTimeBetweenSpawnsMinimum;
+            }
+            maxTimeBetweenSpawns -= timeChangeMaximum;
+            if (maxTimeBetweenSpawns < maxTimeBetweenSpawnsMinimum)
+            {
+                maxTimeBetweenSpawns = maxTimeBetweenSpawnsMinimum;
+            }
+        }
         timeSinceLastSpawn += Time.fixedDeltaTime;
+        timeSinceLastIncrease += Time.fixedDeltaTime;
     }
 }
